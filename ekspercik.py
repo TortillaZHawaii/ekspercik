5dca38b76166eac41a0e534360ad4b01a18ec6af
Dawid Wysocki <tortillazhawaii@gmail.com>
2023-12-07 17:42:20 +0100

    Add GPT4 support and persistent storage

--- ekspercik.py
+++ ekspercik.py
@@ -7,19 +7,21 @@ from langchain.chat_models import ChatOpenAI
 
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
+
 from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OllamaEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password", disabled=True)
+    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
     ollama_model_name = st.text_input("Ollama Model Name", key="langchain_search_ollama_model_name", value="mistral:7b")
-    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    # "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
-    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/TortillaZHawaii/ekspercik)"
+    "[OG Repo](https://codespaces.new/streamlit/llm-examples)"
 
 st.title("🔎 Ekspercik")
 
@@ -30,9 +32,24 @@ uploaded_file = st.file_uploader(
     accept_multiple_files=False,
 )
 
+if not ollama_model_name and not openai_api_key:
+    st.info("Prosze podaj nazwę modelu Ollama lub klucz OpenAPI w sidebarze, aby kontynuować")
+    st.stop()
+
+if ollama_model_name:
+    persist_directory = f"./data/db_{ollama_model_name}"
+    embeddings = OllamaEmbeddings(model=ollama_model_name)
+else:
+    persist_directory = f"./data/db_openai"
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key, model="text-embedding-ada-002")
+
+if "db" not in st.session_state:
+    st.session_state["db"] = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+
 if uploaded_file:
     if st.session_state.get("uploaded_file", None) != uploaded_file:
         st.session_state["uploaded_file"] = uploaded_file
+
         with st.spinner("📥 Pobieram..."):
             tmp_location = f"/tmp/{uploaded_file.name}"
             with open(tmp_location, "wb") as f:
@@ -43,41 +60,32 @@ if uploaded_file:
             )
             raw_documents = loader.load()
         with st.spinner("🔪 Dzielę na zdania..."):
-            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            text_splitter = CharacterTextSplitter(chunk_size=400, chunk_overlap=120)
             documents = text_splitter.split_documents(raw_documents)
         with st.spinner("🔎 Zapisuje w bazie..."):
-            embeddings = OllamaEmbeddings(model=ollama_model_name)
-            db = Chroma.from_documents(documents, embeddings)
+            db: Chroma = st.session_state["db"]
+            db.add_documents(documents)
+            db.persist()
             st.session_state["db"] = db
             st.success("🎉 Gotowe!")
 
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, I'm a chatbot who can search PDF. How can I help you?"}
+        {"role": "assistant", "content": "Heja, jestem chatbotem, który czyta PDFy. Jak mogę Ci pomóc?"}
     ]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+if prompt := st.chat_input(placeholder="Podsumuj wykład w jednym zdaniu"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-
-    if not ollama_model_name:
-        st.info("Please specify Ollama model to continue to continue.")
-        st.stop()
 
     if ollama_model_name:
         llm = ChatOllama(model=ollama_model_name)
     else:
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
-
-    if st.session_state.get("uploaded_file", None) is None:
-        st.info("Please upload a PDF to continue.")
-        st.stop()
-
-
+        llm = ChatOpenAI(model_name="gpt-4", openai_api_key=openai_api_key, streaming=True)
 
     memory = ConversationBufferMemory(
         llm=llm,
@@ -86,6 +94,7 @@ if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
         input_key='question', output_key='answer',
     )
     db = st.session_state["db"]
+
     retriever= db.as_retriever(
         search_type="similarity", k=5, return_metadata=True
     )
@@ -104,8 +113,8 @@ if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
         response = result["answer"]
         sources = result["source_documents"]
 
+        # write each source document in a new line
         st.session_state.messages.append({"role": "assistant", "content": result["answer"]})
-
-        st.write(response)
-        st.write("Źródła:")
-        st.write(sources)
+        st.session_state.messages.append({"role": "assistant", "content": "📚 Oto źródła:"})
+        for source in sources:
+            st.session_state.messages.append({"role": "assistant", "content": source})
